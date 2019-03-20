3b6ce9d2ffa762f6917c3935846a77abb655d12f
Alixluck <836456055@qq.com>
2019-03-20 19:48:10 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+/**
+ * @author Alix
+ * @create 2019-03-20 12:59
+ */
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
+        for (int count = 0; count < 100000000; count++) {
+            System.out.println("count = " + count);
+        }
+
+        for (int t = 0; t < 100; t++) {
+            System.out.println("t = " + t);
+        }
+    }
+
+
+}
